851a74927b545a7743d67f30014a5b306d45e585
kamir222 <katia.amir@gmail.com>
2019-06-23 15:56:01 -0400

    Change color

--- pages/second.js
+++ pages/second.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default () => (
   <div>
-    <h1 style={{color: "blue"}}>Second Page</h1>
+    <h1 style={{color: "pink"}}>Second Page</h1>
     <Link href="/index">
       <a>Go back!</a>
     </Link>
